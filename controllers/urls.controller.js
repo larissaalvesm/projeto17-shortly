bf352f93717839ecae940f8d9ea733d1d4ec9836
larissa <larissaalves1102@gmail.com>
2023-05-20 01:18:54 -0300

    fix: status retornado ao salvar  uma url encurtada

--- controllers/urls.controller.js
+++ controllers/urls.controller.js
@@ -18,7 +18,7 @@ export async function shortenUrl(req, res) {
             "id": urlInfos.rows[0].id,
             "shortUrl": urlInfos.rows[0].shortUrl
         }
-        res.send(body);
+        res.status(201).send(body);
 
     }
     catch (err) {
